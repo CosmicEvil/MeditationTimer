342408dff18ad66a579313c497517408e16104dc
Wouter Massie <massie@adsomenoise.com>
2019-11-21 13:28:17 +0100

    Updates

---
+++ precache-manifest.eec1d5accf1eb3f984b9f5b8d3fc75bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1747785364e1cb49a067fdd7fe3246a",
+    "url": "/MeditationTimer/index.html"
+  },
+  {
+    "revision": "9677217be698d7b87186",
+    "url": "/MeditationTimer/static/css/main.6c8c9376.chunk.css"
+  },
+  {
+    "revision": "4c6de5f28d1434c0c301",
+    "url": "/MeditationTimer/static/js/2.65755673.chunk.js"
+  },
+  {
+    "revision": "9677217be698d7b87186",
+    "url": "/MeditationTimer/static/js/main.3c438206.chunk.js"
+  },
+  {
+    "revision": "476f80ec22c583288876",
+    "url": "/MeditationTimer/static/js/runtime-main.cf41da2a.js"
+  }
+]);
